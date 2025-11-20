7d7b5ba2ab51fa78e84af9302d300f28108ff7de
Andre Luiz Polzin <andrelpolzin@gmail.com>
2025-11-20 15:18:59 -0300

    Updating about section

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -16,24 +16,29 @@ const About = () => {
       <div className="flex flex-col lg:flex-row items-center justify-evenly h-fit p-8 md:p-10 gap-5 lg:gap-8">
         <div className="flex flex-col gap-7 text-white order-2 lg:w-[65%] text-lg md:text-2xl text-justify font-serif md:leading-relaxed">
           <p>
-            Hello, there! My name is André and I'm a self-taught software
-            developer with over 1 year of experience working with a team as a full-stack developer 
-            but I have been on the journey for over 3 years now. I was
-            born and raised in Brazil, but in March 2019 I moved to San
-            Francisco, California, to study English. However, after living there for a few
-            months, I was not only learning a language, but got heavily influenced by the local tech environment. So I
-            decided to start coding for fun and it soon became a habit for me.
+            Hello there! My name is André, and I'm a self-taught software
+            developer with over one year of experience working with a team as a
+            full-stack developer, although I've been on this journey for more
+            than three years now. I was born and raised in Brazil, but in March
+            2019 I moved to San Francisco, California, to study English.
+            However, after living there for a few months, I was not only
+            learning a new language but also becoming heavily influenced by the
+            local tech environment. So I decided to start coding for fun, and it
+            soon became a habit.
           </p>
           <p>
-            Back in Brazil, I got my Bachelor's degree in Business Administration, on December
-            2018, but after graduating I felt I wanted to work with something different from what I had studied.
-            My gut was telling me I needed to work with something more technical that would constantly challenge me 
-            to study and learn new things. So, living in america helped me to fill this gap.
+            Back in Brazil, I earned my Bachelor's degree in Business
+            Administration in December 2018, but after graduating, I felt I
+            wanted to work with something different from what I had studied. My
+            gut was telling me I needed to work with something more
+            technical—something that would constantly challenge me to study and
+            learn new things. Living in America helped me fill that gap.
           </p>
           <p>
-            Althoug I like to stay inside reading or watching TV shows or Counter Strike, during my free time I 
-            love enjoying the outside as well. It could be a
-            simple hike, a nice roadtrip, or go fishing with friends and family. 
+            Although I enjoy staying indoors reading, watching TV shows, or
+            playing Counter-Strike, during my free time I also love being
+            outside. It could be a simple hike, a nice road trip, or going
+            fishing with friends and family.
           </p>
         </div>
         <img
